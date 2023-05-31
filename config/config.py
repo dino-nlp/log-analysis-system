169c8e15ed28be2c6d37c71a41eb77403dccf3f4
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2023-05-31 13:58:14 +0700

    initial project

---
+++ config/config.py
@@ -0,0 +1,81 @@
+import logging
+import sys
+from pathlib import Path
+
+import mlflow
+from rich.logging import RichHandler
+
+# Assets
+BGL_LOG_URL = (
+    "https://raw.githubusercontent.com/logpai/loghub/master/BGL/BGL_2k.log"
+)
+
+# Directories
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+STORES_DIR = Path(BASE_DIR, "stores")
+LOGS_DIR = Path(BASE_DIR, "logs")
+DRAIN3_DIR = Path(BASE_DIR, "drain3")
+
+# Stores
+MODEL_REGISTRY = Path(STORES_DIR, "model")
+BLOB_STORE = Path(STORES_DIR, "blob")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_REGISTRY.mkdir(parents=True, exist_ok=True)
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+BLOB_STORE.mkdir(parents=True, exist_ok=True)
+DRAIN3_DIR.mkdir(parents=True, exist_ok=True)
+
+# MLFlow model registry
+mlflow.set_tracking_uri("file://" + str(MODEL_REGISTRY.absolute()))
+
+# Drain3
+DRAIN3_CONFIG = Path(DRAIN3_DIR, "drain3.ini")
+DRAIN3_FILE_PERSISTENCE = Path(DRAIN3_DIR, "drain3_state.bin")
+
+# Logger
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger()
+logger.handlers[0] = RichHandler(markup=True)
